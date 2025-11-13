2c822878167769a4a6cf81e1c63344b5be426017
ferielziani <ferielziani1607@gmail.com>
2025-11-13 14:11:29 -0500

    Q1-Q3

---
+++ exe2.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main (){
+print ('Entrez votre âge:');
+String? input = stdin.readLineSync();
+try {
+  int age = int.parse(input!);
+  print('Vous avez $age ans.');
+} on FormatException {
+  print('Erreur: Veuillez entrer un nombre valide pour l\'âge.');
+}
+}
